ef210a13e2580f51328e20d96611c865652e1294
Hovhannes Sahakyan <syanhovhannes@gmail.com>
2019-06-23 16:48:27 +0400

    [add] canvas-container and pannel bar

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+function onRerender() {
+  let width = document.getElementById('canvas-width').value;
+  let height = document.getElementById('canvas-height').value;
+
+  console.log({width, height});
+}
---
+++ src/js/maze.js
@@ -0,0 +1,9 @@
+function setup() {
+  let canvas = createCanvas(600 ,600);
+  canvas.parent('maze-container');
+}
+
+
+function draw() {
+  background(51);
+}
